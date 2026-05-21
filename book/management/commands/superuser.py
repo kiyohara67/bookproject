41ca2f7f8c7c595d5e9fc422e832888a2c98e0f1
kiyohara67 <hiroto.kiyohara@van-nw.jp>
2026-05-21 10:54:14 +0900

    fix syantax superuser

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='your_name').exists():
             User.objects.create_superuser(
-                username='your_name'
-                email=''
-                password='your_password'
+                username='your_name',
+                email='',
+                password='your_password',
             )
